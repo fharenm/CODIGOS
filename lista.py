e3af83d0decbab35489dfa5c55be1b1e406c46e4
Margaritavasquez1 <nolosetalvez9@gmail.com>
2024-02-07 14:13:02 -0600

    lista.py

--- lista.py
+++ lista.py
@@ -28,12 +28,14 @@ while seguir:
     elif opcion == 2:
         print("Leer (mostrar) carreras")
         for carrera in carreras:
-            print("- Nombre :" + carrera["carrera"])
+         print("- Nombre :" + carrera["carrera"])
+         print("-----------------------------------")
     elif opcion == 3:
         carreraActualizar = input("Ingrese nombre de la carrera : ")
         print("1.ACTUALIZAR NOMBRE DE LA CARRERA ")
-        print("2.ACTUALIZAR CONTENIDO DE LA CARRERA")
-        print("3.ELIMINAR CONTENIDO DE LA CARRERA")
+        print("2.AGREGAR CLASE  DE LA CARRERA")
+        print("3.ELIMINAR CLASE DE LA CARRERA")
+        print("4.ACTUALIZAR CLASE DE LA CARRERA")
         print("----------------------------------")
         opcion2 = int(input("ingrese su opcion :"))
         if opcion2==1:
@@ -60,6 +62,17 @@ while seguir:
                      carrera["clase"].remove(borrarclase)
                      print("la clase se elimino")
                      print("----------------------------------")
+        elif opcion2==4:
+            actualizarclase=input("Ingrese la clase que desea actualizar")
+            nombrenuevoclase=input("Ingrese el nuevo nombre de la clase ")
+            for carrera in carreras:
+                if carrera["carrera"] == carreraActualizar and "clase" in carrera:
+                    if actualizarclase in carrera["clase"]:
+                     carrera["clase"]=[nombrenuevoclase]
+                    else:
+                     carrera["clase"].append(nombrenuevoclase)
+                    print("se actualizo la clase correctamente ")
+                    print("----------------------------------")
     elif opcion == 4:
         carreraBorrar = input("Ingrese nombre de la carrera : ")
         indice = 0
